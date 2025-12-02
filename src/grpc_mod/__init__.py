eb34eac3248e9ed62416e3194480d630bc883a59
kurama <paul.zenker.pz@gmail.com>
2025-12-02 11:03:38 +0100

    chore: added converters for note and suer

--- src/grpc_mod/__init__.py
+++ src/grpc_mod/__init__.py
@@ -3,3 +3,5 @@ from .proto.note_pb2 import GetNoteRequest, Note, NotePermission, PostNoteReques
 from .proto.user_pb2_grpc import add_UserServiceServicer_to_server, UserService, UserServiceServicer, UserServiceStub
 from .proto.user_pb2 import User, GetUserRequest, AlterUserRequest, DeleteUserRequest, DeleteUserResponse, PostUserRequest
 from .service import GRPCNoteService
+from .converter.note_entity_converter import to_grpc_note
+from .converter.user_entity_converter import to_grpc_user
