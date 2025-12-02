e181f3a651f23e97005c7d9dce6296896a31885d
kurama <paul.zenker.pz@gmail.com>
2025-12-02 16:02:50 +0100

    feat: added UserService to gRPC Server

--- src/main.py
+++ src/main.py
@@ -1,19 +1,31 @@
 import asyncio
 import grpc
-from grpc_mod import add_NoteServiceServicer_to_server, GRPCNoteService
+from grpc_mod import add_NoteServiceServicer_to_server, GRPCNoteService, GRPCUserService
 from db.repos import NoteRepoABC, NotePostgreRepo
 from db import Database
+from db.repos.user.user import UserRepoABC, UserPostgresRepo
+from grpc_mod.proto.user_pb2_grpc import add_UserServiceServicer_to_server
 
 
 async def serve():
+    # create server 
+    server = grpc.aio.server()
+
+    # connect to database
     db = Database(dsn="postgres://postgres:postgres@localhost:5433/db?sslmode=disable")
     await db.init_db()
-    repo: NoteRepoABC = NotePostgreRepo(db=db)
-    note_service = GRPCNoteService(repo=repo)
 
-    server = grpc.aio.server()
+    # note service
+    repo: NotePostgreRepo = NotePostgreRepo(db=db)
+    note_service = GRPCNoteService(repo=repo)
     add_NoteServiceServicer_to_server(note_service, server)
 
+    # user service
+    user_repo: UserRepoABC = UserPostgresRepo(db=db)
+    user_service = GRPCUserService(user_repo=user_repo)
+    add_UserServiceServicer_to_server(user_service, server)
+
+    # configure server
     listen_addr = "[::]:50051"
     server.add_insecure_port(listen_addr)
     print(f"gRPC server listening on {listen_addr}")
