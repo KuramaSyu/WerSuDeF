7a3120acf76135ee79f44b2a2a822fb695978474
kurama <paul.zenker.pz@gmail.com>
2025-12-02 11:05:01 +0100

    feat: implemented GetUser rpc

--- src/grpc_mod/service.py
+++ src/grpc_mod/service.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+import grpc
 from grpc.aio import ServicerContext
 
 from db.repos import NoteRepoABC
@@ -14,6 +15,7 @@ from grpc_mod import (
 )
 from grpc_mod.converter import to_grpc_note
 from db.repos.user.user import UserRepositoryABC
+from src.grpc_mod.converter.user_entity_converter import to_grpc_user
 
 
 class GRPCNoteService(NoteServiceServicer):
@@ -65,10 +67,10 @@ class GRPCNoteService(NoteServiceServicer):
             NoteEntity(
                 note_id=None,
                 author_id=request.author_id,
-                content=request.content.value,
+                content=request.content,
                 embeddings=[],
                 permissions=[],
-                title=request.title.value,
+                title=request.title,
                 updated_at=datetime.now(),
             )
         )
@@ -83,7 +85,24 @@ class GRPCUserService(UserServiceServicer):
         self.repo = user_repo
 
     async def GetUser(self, request: GetUserRequest, context: ServicerContext) -> User:
-        ...
+        if request.HasField("id"):
+            user_entity = await self.repo.select(user_id=request.id)
+        elif request.HasField("discord_id"):
+            user_entity = await self.repo.select_by_discord_id(discord_id=request.discord_id)
+        else:
+            # Neither id nor discord_id provided
+            context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
+            context.set_details("Either 'id' or 'discord_id' must be provided")
+            return User()
+
+        if user_entity is None:
+            # User not found
+            context.set_code(grpc.StatusCode.NOT_FOUND)
+            context.set_details("User not found")
+            return User()
+        
+        # user found and converted to gRPC User Message
+        return to_grpc_user(user_entity)
 
     async def AlterUser(self, request: AlterUserRequest, context: ServicerContext) -> User:
         ...
