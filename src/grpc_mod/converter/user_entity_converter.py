12b3da29f9a8a6f2517ae4ae77be401cce7c754c
kurama <paul.zenker.pz@gmail.com>
2025-12-02 11:03:47 +0100

    feat: added user converter

---
+++ src/grpc_mod/converter/user_entity_converter.py
@@ -0,0 +1,22 @@
+from google.protobuf.timestamp_pb2 import Timestamp
+
+from db.entities.note.metadata import NoteEntity
+from grpc_mod.proto.note_pb2 import Note, NoteEmbedding, NotePermission
+from grpc_mod import User
+from src.db.entities.user.user import UserEntity
+
+def to_grpc_user(user_entity: UserEntity) -> User:
+    """Converts a UserEntity to a gRPC User message."""
+    assert user_entity.id is not None
+    assert user_entity.discord_id is not None
+    assert user_entity.avatar_url is not None
+
+    return User(
+        id=user_entity.id,
+        discord_id=user_entity.discord_id,
+        avatar_url=user_entity.avatar_url,
+    )
+
+
+
+
