2d6bf2a92c9d776f4c439067617134ed70e93005
kurama <paul.zenker.pz@gmail.com>
2025-12-05 12:34:05 +0100

    api: model can be undefined

--- src/db/entities/note/embedding.py
+++ src/db/entities/note/embedding.py
@@ -1,11 +1,17 @@
 from dataclasses import dataclass
 from typing import Sequence
 
+from api.undefined import *
+
 
 @dataclass
 class NoteEmbeddingEntity:
     """Represents one record of note.embedding which contains the model which craeted the embedding,
     the embedding and the note it belongs to"""
     note_id: int
-    model: str
+    model: UndefinedOr[str]
     embedding: Sequence[float]
+
+
+
+
