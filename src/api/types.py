9f9945ae9dbed6706ff33c8f6f7acf2365d70738
kurama <paul.zenker.pz@gmail.com>
2025-12-09 13:24:05 +0100

    api: added Pagination

--- src/api/types.py
+++ src/api/types.py
@@ -1,4 +1,10 @@
+from dataclasses import dataclass
 from typing import Optional, Callable
 import logging
 
 type LoggingProvider = Callable[[str, Optional[object]], logging.Logger]
+
+@dataclass
+class Pagination:
+    limit: int
+    offset: int
